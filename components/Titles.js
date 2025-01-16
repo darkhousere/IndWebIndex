d59e478fa0cba7ba91c7698a2147bcaf71b0b09b
darkhousere <darculinbebarr61991511@gmail.com>
2025-01-16 21:01:07 +0800

    Update Titles.js

--- components/Titles.js
+++ components/Titles.js
@@ -4,11 +4,11 @@ const Titles = () => (
     <div className={`text-[2rem] my-3 text-center font-normal title-1`}>
       <a
         className="underline hover:no-underline text-orange-400 dark:text-lime-100"
-        href="https://github.com/NowScott/IndWebIndex"
+        href="https://bookmark.pp.ua"
         target="_blank"
         rel="noopener noreferrer"
       >
-        Individual Web Index.
+        Ryan Web Index.
       </a>
     </div>
   </div>
